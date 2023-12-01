9ab25a39b9e2b4a71095388c821b532088c5ef7e
DaehoJang <145766768+DaehoJang@users.noreply.github.com>
2023-12-01 22:12:26 +0900

    Update Motor_control.ino

--- Motor_control.ino
+++ Motor_control.ino
@@ -14,10 +14,15 @@ void loop() {
 
   uint16_t POS_VAL = pow(2, NUM_MOTORS) - 1;
   uint16_t value = random(0, POS_VAL);
-
+//계속 출력값이 16비트가 아닌데 왜 아닐까........... 
+?? *** 오류 수정해야함*****
   for (uint8_t i = 0; i < 16; i++) {
     NUM_ON += (value >> (15 - i)) & 0x01;
   }
+  if(NUM_ON > 10){
+    Serial.println(value);
+  }
 } 
+
   //   컨트롤러  2개 필요함 (모터 제어 핀 16개, 한 컨트롤러당 8개씩 제어)
   //  포인터 써서 16비트를 8비트 8비트로 각 컨트롤러에 
