1caaab6e6b0c9d02338687a4020de86be9e8fe7a
2Taehyun <150802226+2Taehyun@users.noreply.github.com>
2023-12-02 11:23:35 +0900

    Update motor_control_revised.ino

--- motor_control_revised.ino
+++ motor_control_revised.ino
@@ -2,6 +2,9 @@
 
 void setup() {
 Serial.begin(9600);
+while (!Serial) {
+; // 시리얼 포트가 연결될 때까지 기다립니다. 이 코드는 네이티브 USB에서 필요합니다.
+}
 }
 
 void loop() {
@@ -16,7 +19,9 @@ NUM_ON += (value >> (15 - i)) & 0x01;
 }
 
 if (NUM_ON > 10) {
-Serial.print(value, BIN); // 16진수로 출력하기 위해 Serial.print() 사용
+for(int i=15; i>=0; i--) {
+Serial.print((value >> i) & 0x1);
+}
 Serial.println(); // 줄바꿈 추가
 delay(1000);
 }
