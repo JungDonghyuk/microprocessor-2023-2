00feb54ad226124191590dccb0e3f0b1c74a9c06
2Taehyun <150802226+2Taehyun@users.noreply.github.com>
2023-12-02 12:07:05 +0900

    Add files via upload

---
+++ lbyte_hbyte.ino
@@ -0,0 +1,26 @@
+#define NUM_MOTORS 16
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; 
+  }
+}
+
+void loop() {
+  uint16_t NUM_ON = 0; 
+  DDRB = 0xFF;
+
+  uint16_t POS_VAL = pow(2, NUM_MOTORS) - 1;
+  uint16_t value = random(0, POS_VAL);
+
+  for (uint8_t i = 0; i < 16; i++) {
+    NUM_ON += (value >> (15 - i)) & 0x01;
+  }
+
+  if (NUM_ON > 10) {
+   uint8_t lByte = (value >> 8);
+   uint8_t hByte = (value & 0xff);
+    delay(1000);
+}
+}
