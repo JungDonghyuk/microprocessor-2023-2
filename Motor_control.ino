175ec4d5b03d3e6ade44f02072796dc091f92880
DaehoJang <145766768+DaehoJang@users.noreply.github.com>
2023-12-02 12:14:07 +0900

    Update Motor_control.ino

--- Motor_control.ino
+++ Motor_control.ino
@@ -1,28 +1,28 @@
 #define NUM_MOTORS 16
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-    ; 
-  }
+
+while (!Serial) {
+  ; // 시리얼 포트가 연결될 때까지 기다립니다. 이 코드는 네이티브 USB에서 필요합니다.
 }
-// 구현을 위해 loop함수 사용 
-// 실제 코드에서는 loop빼고!!
+}
+
 void loop() {
-  uint16_t NUM_ON = 0; 
-  DDRB = 0xFF;
+uint16_t NUM_ON = 0; // NUM_ON 변수의 타입을 uint16_t로 변경
+DDRB = 0xFF;
 
-  uint16_t POS_VAL = pow(2, NUM_MOTORS) - 1;
-  uint16_t value = random(0, POS_VAL);
-//계속 출력값이 16비트가 아닌데 왜 아닐까........... 
-?? *** 오류 수정해야함*****
-  for (uint8_t i = 0; i < 16; i++) {
-    NUM_ON += (value >> (15 - i)) & 0x01;
-  }
-  if(NUM_ON > 10){
-    Serial.println(value);
-  }
-} 
+uint16_t POS_VAL = pow(2, NUM_MOTORS) - 1;
+uint16_t value = random(0, POS_VAL);
 
-  //   컨트롤러  2개 필요함 (모터 제어 핀 16개, 한 컨트롤러당 8개씩 제어)
-  //  포인터 써서 16비트를 8비트 8비트로 각 컨트롤러에 
+for (uint8_t i = 0; i < 16; i++) {
+NUM_ON += (value >> (15 - i)) & 0x01;
+}
+
+if (NUM_ON > 10) {
+for(int i=15; i>=0; i--) {
+Serial.print((value >> i) & 0x1); //serial.print는 10진수로 표현됨
+}
+Serial.println(); // 줄바꿈 추가
+delay(1000);
+}
+}
